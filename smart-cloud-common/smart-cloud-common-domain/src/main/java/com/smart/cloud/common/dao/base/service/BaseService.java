7600fdddd4c6e25a32ab5ee64cdee203aea65697
marklin <2273990296@qq.com>
2023-05-30 00:35:50 +0800

    [新增功能]-[master 2023-05-29 星期一 22:32]:smart-cloud项目-提交parent相关模块

--- smart-cloud-common/smart-cloud-common-domain/src/main/java/com/smart/cloud/common/dao/base/service/BaseService.java
+++ smart-cloud-common/smart-cloud-common-domain/src/main/java/com/smart/cloud/common/dao/base/service/BaseService.java
@@ -14,5 +14,33 @@ import com.smart.cloud.common.dao.Service;
  * @version: 1.0.0
  * @copyright: Copyright © 2018-2023 SmartCloud Systems Incorporated. All rights reserved.
  */
-public interface BaseService<T> extends Service<T>{
+public interface BaseService<T,ID>  extends Service<T>{
+
+    /**
+     * 查询详情-detail
+     * @param id 参数id
+     * @return 返回结果
+     */
+    T detail(ID id);
+
+    /**
+     * 新增数据-create
+     * @param entity 参数对象
+     * @return 返回结果
+     */
+    boolean create(T entity);
+
+    /**
+     * 删除数据-delete
+     * @param id 参数id
+     * @return 返回结果
+     */
+    boolean delete(ID id);
+
+    /**
+     * 更新数据-update
+     * @param entity 参数对象
+     * @return 返回结果
+     */
+    boolean update(T entity);
 }
--- smart-cloud-common/smart-cloud-common-domain/src/main/java/com/smart/cloud/common/dao/base/service/impl/BaseServiceImpl.java
+++ smart-cloud-common/smart-cloud-common-domain/src/main/java/com/smart/cloud/common/dao/base/service/impl/BaseServiceImpl.java
@@ -8,6 +8,9 @@ import com.smart.cloud.common.dao.base.service.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import java.io.Serializable;
+
 /**
  * @className: com.smart.cloud.common.dao.base.service.impl.BaseServiceImpl
  * @title: 封装SmartCloud项目-BaseServiceImpl类
@@ -21,7 +24,50 @@ import org.springframework.stereotype.Component;
  * @copyright: Copyright © 2018-2023 SmartCloud Systems Incorporated. All rights reserved.
  */
 @Component
-public class BaseServiceImpl<M extends BaseMapper<T>, T> extends ServiceImpl<M, T> implements BaseService<T> {
+public class BaseServiceImpl<M extends BaseMapper<T>, T, ID> extends ServiceImpl<M, T> implements BaseService<T,ID> {
     @Autowired
     private AppRuntimeEnvironment environment;
+
+    @Resource
+    protected M baseMapper;
+
+    /**
+     * 查询详情-detail
+     * @param id 参数id
+     * @return 返回结果
+     */
+    @Override
+    public T detail(ID id) {
+        return this.baseMapper.selectById((Serializable) id);
+    }
+
+    /**
+     * 新增数据-create
+     * @param entity 参数对象
+     * @return 返回结果
+     */
+    @Override
+    public boolean create(T entity) {
+        return this.baseMapper.insert(entity) > 0;
+    }
+
+    /**
+     * 删除数据-delete
+     * @param id 参数id
+     * @return 返回结果
+     */
+    @Override
+    public boolean delete(ID id) {
+        return this.baseMapper.deleteById((Serializable) id) > 0;
+    }
+
+    /**
+     * 更新数据-update
+     * @param entity 参数对象
+     * @return 返回结果
+     */
+    @Override
+    public boolean update(T entity) {
+        return this.baseMapper.updateById(entity) > 0;
+    }
 }
