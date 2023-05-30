db6e4948ba02f2434980bec5c832facbcd839033
marklin <2273990296@qq.com>
2023-05-31 03:25:33 +0800

    [新增功能]-[master 2023-05-30 星期二 21:05]:smart-cloud项目-提交parent相关模块

--- smart-cloud-framework/smart-cloud-coverage/smart-cloud-coverage-audit/src/main/java/com/smart/cloud/coverage/audit/aspect/ApplicationAuditAspect.java
+++ smart-cloud-framework/smart-cloud-coverage/smart-cloud-coverage-audit/src/main/java/com/smart/cloud/coverage/audit/aspect/ApplicationAuditAspect.java
@@ -1,11 +1,20 @@
 package com.smart.cloud.coverage.audit.aspect;
 
 import com.smart.cloud.boot.aop.ApplicationAspect;
+import com.smart.cloud.coverage.audit.annotation.ApplicationAudit;
+import com.smart.cloud.coverage.audit.handler.ApplicationCompareHandler;
+import com.smart.cloud.coverage.audit.support.SpelExpressionAware;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.Ordered;
+import org.springframework.util.StringUtils;
+
+import java.lang.reflect.Method;
 
 /**
  * @className: com.smart.cloud.coverage.audit.aspect.ApplicationAuditAspect
@@ -25,10 +34,13 @@ import org.springframework.core.Ordered;
 @Lazy(false)
 public class ApplicationAuditAspect implements ApplicationAspect {
 
+    protected final ApplicationCompareHandler applicationCompare;
     /**
      * aspect切面-切入点
      */
     @Override
+    @Pointcut("@annotation(com.smart.cloud.coverage.audit.annotation.ApplicationAudit) || " +
+            "@within(com.smart.cloud.coverage.audit.annotation.ApplicationAudit)")
     public void aspectPoint() {
 
     }
@@ -40,8 +52,18 @@ public class ApplicationAuditAspect implements ApplicationAspect {
      * @throws Throwable 异常消息
      */
     @Override
+    @Around("aspectPoint()")
     public Object aspectAround(ProceedingJoinPoint point) throws Throwable {
-        return null;
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method method = signature.getMethod();
+        ApplicationAudit audit = method.getAnnotation(ApplicationAudit.class);
+        if (audit == null) {
+            audit = method.getDeclaringClass().getAnnotation(ApplicationAudit.class);
+        }
+        Object target = SpelExpressionAware.apply(point, StringUtils.hasText(audit.history()) ? audit.history() : audit.example());
+        Object result = point.proceed();
+        applicationCompare.compare(audit,point,target);
+        return result;
     }
 
     @Override
