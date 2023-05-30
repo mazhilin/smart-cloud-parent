81d362adbba0b2f10e9eec731b42e24a0ef09ebc
marklin <2273990296@qq.com>
2023-05-31 02:57:48 +0800

    [新增功能]-[master 2023-05-30 星期二 21:05]:smart-cloud项目-提交parent相关模块

--- smart-cloud-framework/smart-cloud-coverage/smart-cloud-coverage-audit/src/main/java/com/smart/cloud/coverage/audit/configuration/EnableAuditAutoConfiguration.java
+++ smart-cloud-framework/smart-cloud-coverage/smart-cloud-coverage-audit/src/main/java/com/smart/cloud/coverage/audit/configuration/EnableAuditAutoConfiguration.java
@@ -1,10 +1,19 @@
 package com.smart.cloud.coverage.audit.configuration;
 
 import com.smart.cloud.coverage.audit.aspect.ApplicationAuditAspect;
+import com.smart.cloud.coverage.audit.handler.ApplicationAuditHandler;
+import com.smart.cloud.coverage.audit.handler.ApplicationCompareHandler;
+import com.smart.cloud.coverage.audit.handler.event.ApplicationAuditEventHandler;
 import com.smart.cloud.coverage.audit.support.SpelExpressionAware;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.Optional;
 
 /**
  * @className: com.smart.cloud.coverage.audit.configuration.EnableAuditAutoConfiguration
@@ -22,4 +31,18 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAutoConfiguration
 @Import({ ApplicationAuditAspect.class, SpelExpressionAware.class})
 public class EnableAuditAutoConfiguration {
+
+    /**
+     * 默认注入 javers 的比较器实现
+     * @param optional 注入审计用户来源
+     * @return ApplicationAuditHandler
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    @SuppressWarnings("ALL")
+    public ApplicationCompareHandler applicationCompare(Optional<ApplicationAuditHandler> optional){
+        return new ApplicationAuditEventHandler(optional);
+    }
+
+
 }
