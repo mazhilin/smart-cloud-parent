88d22662f538fee96207d1f39f0eb35232516ce0
marklin <2273990296@qq.com>
2023-06-11 02:49:36 +0800

    [新增功能]-[master 2023-06-11 星期日 02:49]:smart-cloud项目-parent模块组件封装

---
+++ smart-cloud-framework/smart-cloud-datacache/smart-cloud-datacache-boot/src/main/java/com/smart/cloud/datacache/boot/constants/DatacacheType.java
@@ -0,0 +1,17 @@
+package com.smart.cloud.datacache.boot.constants;
+
+/**
+ * @className: com.smart.cloud.datacache.boot.constants.DatacacheType
+ * @title: 封装SmartCloud项目-DatacacheType类
+ * @description: <p>
+ *         SmartCloud项目-DatacacheType
+ *         </p>
+ * @content: DatacacheType
+ * @author: Powered by marklin
+ * @datetime: 2023-06-08 00:52
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 SmartCloud Systems Incorporated. All rights reserved.
+ */
+public enum  DatacacheType {
+    REMOTE, LOCAL, BOTH
+}
