a09c83f8362be0a851e5e6e3af7598decf1a4eba
marklin <2273990296@qq.com>
2023-07-12 21:23:08 +0800

    [新增功能]-[master 2023-07-11 星期四 21:04]:smart-cloud项目-parent模块组件封装

---
+++ smart-cloud-common/smart-cloud-common-center/src/main/java/com/smart/cloud/common/api/base/server/BaseApiServer.java
@@ -0,0 +1,14 @@
+package com.smart.cloud.common.api.base.server;
+
+/**
+ * @className: com.smart.cloud.common.api.base.server.BaseApiServer
+ * @projectName: 封装SmartCloud项目-BaseApiServer类
+ * @module: SmartCloud项目-BaseApiServer类，主要位于BaseApiServer模块的业务场景
+ * @content: BaseApiServer类，主要用于完成BaseApiServer的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-07-12 21:18
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 SmartCloud Systems Incorporated. All rights reserved.
+ */
+public class BaseApiServer implements ApiServer{
+}
---
+++ smart-cloud-common/smart-cloud-common-web/src/main/java/com/smart/cloud/common/module/base/consumer/BaseApiConsumer.java
@@ -0,0 +1,14 @@
+package com.smart.cloud.common.module.base.consumer;
+
+/**
+ * @className: com.smart.cloud.common.module.base.consumer.BaseApiConsumer
+ * @projectName: 封装SmartCloud项目-BaseApiConsumer类
+ * @module: SmartCloud项目-BaseApiConsumer类，主要位于BaseApiConsumer模块的业务场景
+ * @content: BaseApiConsumer类，主要用于完成BaseApiConsumer的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2023-07-12 21:07
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2023 SmartCloud Systems Incorporated. All rights reserved.
+ */
+public class BaseApiConsumer {
+}
